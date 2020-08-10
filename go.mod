2f3d1e550fe7f5adf5629ec4e408453d9b3dfd62
CristianRamirez <cristian.ramirez@jaak-it.com>
2020-08-10 12:24:15 -0500

    Add models

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jaak-it/lambda-commons
 
 go 1.14
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
---
+++ models/response.go
@@ -0,0 +1,15 @@
+package models
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+type ResponseError struct {
+	Response
+	Error interface{} `json:"error"`
+}
+
+type ResponseSuccess struct {
+	Response
+	Payload interface{} `json:"payload"`
+}
