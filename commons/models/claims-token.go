46d2451b4b896b10fc99e794c5925e9457cae27c
CristianRamirez <cristian.ramirez@jaak-it.com>
2020-08-24 16:12:43 -0500

    change model claims token

--- commons/models/claims-token.go
+++ commons/models/claims-token.go
@@ -3,7 +3,7 @@ package models
 import "github.com/dgrijalva/jwt-go"
 
 type ClaimsToken struct {
-	Username string `json:"cognito:username"`
-	Email    string `json:"email"`
-	jwt.StandardClaims
+    Username string   `json:"username"`
+    Roles    []string `json:"cognito:groups"`
+    jwt.StandardClaims
 }
